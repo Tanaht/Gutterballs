0a02c80f63613a90ddecd72b434102ddeeb039ef
Kakawuette <Kakawuette@users.noreply.github.com>
2016-12-01 16:41:35 +0100

    commentaire SalleChaussure v2

--- se.gutterballs/src/part2/SalleChaussure.java
+++ se.gutterballs/src/part2/SalleChaussure.java
@@ -1,18 +1,24 @@
 package part2;
 
 public class SalleChaussure {
+	/**
+	* fait prendre une paire de chaussure au client, cela prend un certain temps
+	* @param client devant prendre une paire
+	*/
 	public synchronized void prendre(Client client) {
 		try {
 			Thread.sleep(300);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		client.putChaussures();
 		
 	}
-	
+	/**
+	*  fait rendre sa paire de chaussure au client, cela prend un certain temps
+	* @ param client  devant rendre sa paire de chaussures
+	*/
 	public synchronized void rendre(Client client) {
 		try {
 			Thread.sleep(300);
