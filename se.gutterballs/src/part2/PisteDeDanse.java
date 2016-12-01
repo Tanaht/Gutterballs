203a5556628ee5f41958f8d9ba7a9b0df0b85f2f
Kakawuette <Kakawuette@users.noreply.github.com>
2016-12-01 16:40:48 +0100

    commentaire pisteDeDanse v2

--- se.gutterballs/src/part2/PisteDeDanse.java
+++ se.gutterballs/src/part2/PisteDeDanse.java
@@ -3,15 +3,30 @@ package part2;
 import java.util.LinkedList;
 
 public class PisteDeDanse {
+	/**
+	* true si aucune piste n'est libre, si non false
+	*/
 	private boolean pisteLibre;
+	/**
+	* groupe prioritaire pouvant sortir de la piste
+	*/
 	private Groupe prochainGroupeQuiJoue;
+	/**
+	* Fifo sur les groupes qui sont arrives sur la piste de danse
+	*/
 	private LinkedList<Groupe> fifo;
-	
+	/**
+	* constructeur
+	*/
 	public PisteDeDanse() {
 		this.pisteLibre = true;
 		this.fifo = new LinkedList<>();
 	}
-	
+	/**
+	* fait danser le client tant qu'aucune piste n'est disponible, 
+	* si le groupe du client est le plus ancien sur la piste il sera le premier a sortir
+	* @param client allant danser
+	*/
 	public synchronized void echauffement(Client client) {
 		if(!this.fifo.contains(client.getGroupe())) {
 			System.out.println("ON EMPILE ----------> " + client.getGroupe());
@@ -22,7 +37,6 @@ public class PisteDeDanse {
 			try {
 				wait();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -41,13 +55,17 @@ public class PisteDeDanse {
 		System.out.println(client + "" + client.getGroupe() + "[Fin Echauffement]");
 		notifyAll();
 	}
-	
+	/**
+	* prmet de notifier la liberation d'une piste
+	*/
 	public synchronized void pisteLiberee() {
 		System.out.println("[PisteDeDanse][Une piste est libre]");
 		this.pisteLibre = true;
 		notify();
 	}
-	
+	/**
+	* permet de prendre la piste venant d'etre liberee
+	*/
 	public synchronized void pistesOccupee() {
 		System.out.println(Thread.currentThread().getName() + "[PisteDeDanse][les pistes sont occupees]");
 		this.pisteLibre = false;
