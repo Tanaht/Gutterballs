732cd115f7fa907caadb847d53aeeffcf9a282b0
Kakawuette <Kakawuette@users.noreply.github.com>
2016-12-01 15:06:22 +0100

    commentaire Guichet v1

--- se.gutterballs/src/part1/Guichet.java
+++ se.gutterballs/src/part1/Guichet.java
@@ -3,17 +3,33 @@ package part1;
 import java.util.Random;
 
 public class Guichet {
-	
+	/**
+	* numero d'identification du guichet
+	*/
 	private int ids;
+	/**
+	* nombre de client que doit atteindre un groupe avant d'etre complet
+	*/
 	private int capacite;
+	/**
+	* groupe en cours de creation
+	*/
 	private Groupe groupe;
 	
+	/**
+	* constructeur
+	* @param capaciteGroupe capacite du groupe a atteindre
+	*/
 	public Guichet(int capaciteGroupe) {
 		this.capacite = capaciteGroupe;
 		this.ids = 0;
 		this.groupe = new Groupe(this.ids++, this.capacite);
 	}
 	
+	/**
+	* permet a un client de s'inscrire dans le groupe qui est en cours de creation, cela prend un certain temps
+	* @param client voulant s'inscrire
+	*/
 	public synchronized void inscription(Client client) {
 		if(this.groupe.isComplete())
 			this.groupe = new Groupe(this.ids++, this.capacite);
@@ -28,6 +44,10 @@ public class Guichet {
 		this.groupe.addClient(client);
 	}
 
+	/**
+	* permet a un client de regler sa note, cela prend un certain temps
+	* @param client voulant payer sa partie
+	*/
 	public synchronized void payer(Client client) {
 		try {
 			Thread.sleep(200);
