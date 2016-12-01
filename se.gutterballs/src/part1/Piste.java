77a7ad1ebded54f110da089955993d7393fc28d6
Kakawuette <Kakawuette@users.noreply.github.com>
2016-12-01 15:09:01 +0100

    commentaire piste v1

--- se.gutterballs/src/part1/Piste.java
+++ se.gutterballs/src/part1/Piste.java
@@ -3,17 +3,33 @@ package part1;
 import java.util.Random;
 
 public class Piste {
+	/**
+	* numero d'identification de la piste
+	*/
 	private int id;
 	
+	/**
+	* constructeur
+	* @param id identificateur de la piste
+	*/
 	public Piste(int id) {
 		this.id = id;
 	}
 	
+	/**
+	* fait venir le client sur la piste et le fait attendre les autres membre de son groupe
+	* une fois que tout le groupe est sur la piste, il joue
+	* @param client
+	*/
 	public void utiliser(Client client) {
 		client.getGroupe().addClientSurPiste(client);
 		client.getGroupe().waitAllSurPiste(client);
 		this.jouer();
 	}
+	
+	/**
+	* methode simulant le fait de jouer une partie, cela prend un certain temps
+	*/
 	public void jouer() {
 		try {
 			//Il faut qu'une partie dure plus longtemps que le temps que va mettre le groupe suivant pour s'inscrire et prendre leurs chaussures.
