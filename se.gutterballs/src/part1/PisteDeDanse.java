00a4e349ca91661e642104e45f7fe48f10c3f8ac
Kakawuette <Kakawuette@users.noreply.github.com>
2016-12-01 15:15:55 +0100

    commentaire piste v1

--- se.gutterballs/src/part1/PisteDeDanse.java
+++ se.gutterballs/src/part1/PisteDeDanse.java
@@ -1,19 +1,29 @@
 package part1;
 
 public class PisteDeDanse {
+	/**
+	* true si aucune piste n'est libre, si non false
+	*/
 	private boolean pisteLibre;
+	/**
+	* groupe prioritaire pouvant sortir de la piste
+	*/
 	private Groupe prochainGroupeQuiJoue;
-	
+	/**
+	* constructeur
+	*/
 	public PisteDeDanse() {
 		this.pisteLibre = true;
 	}
-	
+	/**
+	* fait dans le client tant qu'aucune piste n'est disponible
+	* @param client allant danser
+	*/
 	public synchronized void echauffement(Client client) {
 		while(!pisteLibre && client.getGroupe() != this.prochainGroupeQuiJoue) {
 			try {
 				wait();
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -25,12 +35,17 @@ public class PisteDeDanse {
 		notifyAll();
 	}
 	
+	/**
+	* prmet de notifier la liberation d'une piste
+	*/
 	public synchronized void pisteLiberee() {
 		System.out.println("[PisteDeDanse][Une piste est libérée]");
 		this.pisteLibre = true;
 		notify();
 	}
-	
+	/**
+	* permet de prendre la piste venant d'etre liberee
+	*/
 	public synchronized void pistesOccupee() {
 		System.out.println(Thread.currentThread().getName() + "[PisteDeDanse][les pistes sont occupée]");
 		this.pisteLibre = false;
